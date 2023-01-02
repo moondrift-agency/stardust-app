cc4935be1ed3e9901cdd9bf9235adf93436593b8
WilhelmRosental <wilhelm.rosental@ik.me>
2023-01-02 18:15:00 +0100

    mui update and project structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,31 @@
+import * as React from 'react'
 import Head from 'next/head'
-import { AuthProvider } from '../lib/auth'
-import { supabase } from '../lib/supabaseClient'
+import { AppProps } from 'next/app'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import { CacheProvider, EmotionCache } from '@emotion/react'
+import theme from '../src/theme'
+import createEmotionCache from '../src/createEmotionCache'
 
-import type { AppProps } from 'next/app'
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache()
 
-//theme
-import { Layout } from '../layouts/Layout'
-import { Global } from '@emotion/react'
-import { globalStyles } from '../styles/global'
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
+}
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
   return (
-    <>
-      <Global styles={globalStyles} />
+    <CacheProvider value={emotionCache}>
       <Head>
-        <title>Stardust</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-
-      <Layout>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
         <Component {...pageProps} />
-      </Layout>
-      {/* <AuthProvider supabase={supabase}>
-        
-      </AuthProvider> */}
-    </>
+      </ThemeProvider>
+    </CacheProvider>
   )
 }
